ba9b6dea1bf59e5aac8bed17df9f3596f05f8000
Daryl <ng.daryl@gmail.com>
2021-01-20 08:32:22 +0100

    Included DB

--- PhotographyOnlineStore.WebUI/Controllers/HomeController.cs
+++ PhotographyOnlineStore.WebUI/Controllers/HomeController.cs
@@ -1,18 +1,56 @@
-﻿using System;
+﻿using PhotographyOnlineStore.Core.Contracts;
+using PhotographyOnlineStore.Core.Models;
+using PhotographyOnlineStore.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PhotographyOnlineStore.WebUI.Controllers
+namespace LexShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            return View();
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
+        public ActionResult Index(string Category = null)
+        {
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            System.Diagnostics.Debug.WriteLine("products: " + products.Count);
+            return View(model);
         }
 
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
