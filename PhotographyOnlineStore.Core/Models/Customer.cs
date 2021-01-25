bc8aecac2c450e7d7809d4a3802e7ae920977f78
sobhi@cloudulu.com <sobhi@cloudulu.com>
2021-01-25 18:05:37 +0100

    move in

---
+++ PhotographyOnlineStore.Core/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using PhotographyOnlineStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoographyOnlineStore.Core.Models
+{
+    public class Customer : BaseEntity
+    {
+        // Create this class and add some in info that we are going to store later..
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
+
