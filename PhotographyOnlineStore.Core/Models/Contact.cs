aef1ec2a1b6a825af7cc6be8ab3bd6694bacb49e
sobhi@cloudulu.com <sobhi@cloudulu.com>
2021-01-25 17:53:43 +0100

    trial

---
+++ PhotographyOnlineStore.Core/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoographyOnlineStore.Core.Models
+{
+    [Table("Contacts")]
+    public class Contact
+    {
+        public int Id { get; set; }
+        [StringLength(100)]
+        [Required]
+        public string FullName { get; set; }
+        [StringLength(100), Required]
+        public string Email { get; set; }
+        [StringLength(1000), Required]
+        public string Message { get; set; }
+        [StringLength(15), Required]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ PhotographyOnlineStore.WebUI/Migrations/202101251132402_Contact.cs
@@ -0,0 +1,29 @@
+namespace PhotographyOnlineStore.WebUI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Contact : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FullName = c.String(nullable: false, maxLength: 100),
+                        Email = c.String(nullable: false, maxLength: 100),
+                        Message = c.String(nullable: false, maxLength: 1000),
+                        PhoneNumber = c.String(nullable: false, maxLength: 15),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contacts");
+        }
+    }
+}
