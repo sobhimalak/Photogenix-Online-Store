0ab18e8271b466431254b1be3dd727497b624497
sobhi@cloudulu.com <sobhi@cloudulu.com>
2021-01-26 14:59:59 +0100

    solved Photoography

--- PhotographyOnlineStore.Core/Properties/AssemblyInfo.cs
+++ PhotographyOnlineStore.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhotoographyOnlineStore.Core")]
+[assembly: AssemblyTitle("PhotographyOnlineStore.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PhotoographyOnlineStore.Core")]
+[assembly: AssemblyProduct("PhotographyOnlineStore.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
