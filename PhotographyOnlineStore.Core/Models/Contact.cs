068b80297d2cec1d411f44119a659d665f14a196
sobhi@cloudulu.com <sobhi@cloudulu.com>
2021-01-26 12:07:08 +0100

    Added contact page

--- PhotographyOnlineStore.Core/Models/Contact.cs
+++ PhotographyOnlineStore.Core/Models/Contact.cs
@@ -1,25 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PhotoographyOnlineStore.Core.Models
 {
-    [Table("Contacts")]
     public class Contact
     {
-        public int Id { get; set; }
-        [StringLength(100)]
+        [Required, Display(Name = "Sender Name")]
+        public string SenderName { get; set; }
+        [Required, Display(Name = "Sender Email"), EmailAddress]
+        public string SenderEmail { get; set; }
         [Required]
-        public string FullName { get; set; }
-        [StringLength(100), Required]
-        public string Email { get; set; }
-        [StringLength(1000), Required]
         public string Message { get; set; }
-        [StringLength(15), Required]
-        public string PhoneNumber { get; set; }
     }
 }
